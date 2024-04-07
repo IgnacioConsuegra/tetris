4051878e2ea5f5b504d8b6ad04e290caf187de4a
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2024-04-07 20:04:04 +0200

    Now, it solidify

--- script.js
+++ script.js
@@ -75,6 +75,17 @@ function checkCollision() {
     })
   })
 }
+function solidifyPiece() {
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if(value === 1 ) {
+        board[y + piece.position.y][x + piece.position.x] = 1;
+      }
+    })
+  })
+  piece.position.x = 0;
+  piece.position.y = 0;
+}
 
 document.addEventListener('keydown', (event) => {
   if(event.key === 'ArrowLeft') {
@@ -93,6 +104,7 @@ document.addEventListener('keydown', (event) => {
     piece.position.y++;
     if(checkCollision()) {
       piece.position.y--;
+      solidifyPiece();
     }
   };
 })
